8832b159072e19c8edb0f08cbb0f89c69ce35aea
DEEPKA RAJ G <111234076+DEEPAK2516@users.noreply.github.com>
2023-11-24 14:49:50 +0530

    script.js

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
-  // Get the parameters from the URL
   const urlParams = new URLSearchParams(window.location.search);
-  
-  // Check if the 'message' parameter is present
+
   if (urlParams.has('message')) {
     const receivedData = urlParams.get('message');
-
-    // Display the received data on the page
     const receivedDataDiv = document.getElementById('receivedData');
     receivedDataDiv.innerHTML = `Received Data: ${receivedData}`;
   } else {
